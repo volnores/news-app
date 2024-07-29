0e085e6a21945133540472a71d4701d9ff69ff15
volnores <volnores2409@mail.ru>
2024-07-29 21:01:32 +0300

    add skeleton

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,15 +3,20 @@ import styles from './styles.module.css';
 import NewsBanner from '../components/NewsBanner/NewsBanner';
 import { getNews } from '../api/apiNews';
 import NewsList from '../NewsList/NewsList';
+import Skeleton from '../components/Skeleton/Skeleton';
 
 const Main = () => {
   const [news, setNews] = useState([]);
 
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     const fetctNews = async () => {
       try {
+        setIsLoading(true);
         const response = await getNews();
         setNews(response.news);
+        setIsLoading(false);
       } catch (error) {
         console.log(error);
       }
@@ -21,8 +26,8 @@ const Main = () => {
 
   return (
     <main className={styles.main}>
-      {news.length > 0 ? <NewsBanner item={news[29]} /> : null}
-      <NewsList news={news} />
+      {news.length > 0 && !isLoading ? <NewsBanner item={news[29]} /> : <Skeleton />}
+      {!isLoading ? <NewsList news={news} /> : <Skeleton />}
     </main>
   );
 };
